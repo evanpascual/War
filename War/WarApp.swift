446deb07361a1024ff22e959865360711514618e
evanpascual <pascual.evan@gmail.com>
2022-06-17 18:44:35 -0700

    Initial Commit

---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Evan Pascual on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
